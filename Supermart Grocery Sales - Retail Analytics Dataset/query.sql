75c4a405296ef5b82f322a0946e49def9cdada57
Iqrar Agalosi Nureyza <iqrar.agalosi@ui.ac.id>
2022-06-24 07:35:34 +0700

    Change comment format

--- Supermart Grocery Sales - Retail Analytics Dataset/query.sql
+++ Supermart Grocery Sales - Retail Analytics Dataset/query.sql
@@ -1,6 +1,8 @@
 -- queries by Iqrar Agalosi Nureyza
 
--- Check the top 10 customer with the most order
+/*
+	Check the top 10 customer with the most order
+*/
 SELECT customer_name, COUNT(*) AS total_order
 FROM supermart
 GROUP BY customer_name
@@ -8,8 +10,10 @@ ORDER BY total_order DESC
 LIMIT 10;
 
 
--- See the total sales for each category on west region during 2017,
--- order it from the highest sales
+/*
+	See the total sales for each category on west region during 2017,
+	order it from the highest sales
+*/
 SELECT category, SUM(sales) as total_sales
 FROM supermart
 WHERE EXTRACT('year' from order_date) = 2017
@@ -18,7 +22,9 @@ GROUP BY category
 ORDER BY total_sales DESC;
 
 
--- What is the average sales for beverages during 4th quarter of 2018?
+/*
+	What is the average sales for beverages during 4th quarter of 2018?
+*/
 SELECT AVG(sales) AS beverages_sales_average
 FROM supermart
 WHERE EXTRACT('year' from order_date) = 2018
@@ -26,7 +32,9 @@ AND EXTRACT('month' from order_date) >= 10
 AND category = 'Beverages';
 
 
--- Check the average and total profits of snacks for each month during 2016
+/*
+	Check the average and total profits of snacks for each month during 2016
+*/
 SELECT EXTRACT('year' from order_date) as order_year,
 	EXTRACT('month' from order_date) as order_month,
 	AVG(profit) AS profit_avg,
@@ -36,7 +44,9 @@ WHERE EXTRACT('year' from order_date) = 2016
 GROUP BY order_year, order_month;
 
 
--- Check what cities that never order Masalas on May 2017
+/*
+	Check what cities that never order Masalas on May 2017
+*/
 SELECT city 
 FROM supermart
 WHERE city NOT IN (	
@@ -48,8 +58,10 @@ WHERE city NOT IN (
 );
 
 
--- What is the percentage of ordering amount of Noodles
--- compared to all Snacks category during 2017?
+/*
+	What is the percentage of ordering amount of Noodles
+	compared to all Snacks category during 2017?
+*/
 CREATE OR REPLACE FUNCTION calculate_percentage_subcategory(c text, sub_c text, order_year INT)
 RETURNS FLOAT
 AS
@@ -79,7 +91,9 @@ SELECT calculate_percentage_subcategory('Snacks', 'Noodles', 2017)
 AS "Noodles order percentage 2017 (%)";
 
 
--- When is the highest discount of Bakery category happened in 2015?
+/*
+	When is the highest discount of Bakery category happened in 2015?
+*/
 SELECT order_date, discount
 FROM supermart
 WHERE discount IN (
@@ -92,7 +106,9 @@ AND category = 'Bakery'
 ORDER BY order_date;
 
 
--- How many unique people that have more than two orders in a same day during 2018?
+/*
+	How many unique people that have more than two orders in a same day during 2018?
+*/
 SELECT COUNT(DISTINCT customer_name) AS total_unique_customer
 FROM (
 	SELECT customer_name, order_date, COUNT(*) AS total_order_per_day
